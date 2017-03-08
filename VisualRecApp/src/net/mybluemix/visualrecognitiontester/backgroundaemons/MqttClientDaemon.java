d776611b584831f86e366a4f27235cc866e54e6b
marcodondio <marcodondio@marcos-mbp>
2017-03-09 00:02:54 +0100

    Corretto bug reconnect

--- VisualRecApp/src/net/mybluemix/visualrecognitiontester/backgroundaemons/MqttClientDaemon.java
+++ VisualRecApp/src/net/mybluemix/visualrecognitiontester/backgroundaemons/MqttClientDaemon.java
@@ -8,7 +8,7 @@ import java.util.TimeZone;
 import javax.servlet.ServletContext;
 
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
@@ -23,7 +23,11 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
  *
  */
 // https://eclipse.org/paho/clients/java/
-public class MqttClientDaemon implements Runnable, MqttCallback {
+
+// TODO vedi esempio 1
+// occhio, forse vorrei cleansession false
+// https://github.com/eclipse/paho.mqtt.java/issues/9
+public class MqttClientDaemon implements Runnable, MqttCallbackExtended {
 
 	private ServletContext ctx;
 
@@ -53,7 +57,7 @@ public class MqttClientDaemon implements Runnable, MqttCallback {
 
 	// initialize code is executed by run method, this means
 	// it will be executed by the thread running this class
-	private void initialize() throws MqttException {
+	private void initialize() {
 		// initialize room info
 		ctx.setAttribute("roomLastScan", "no scan yet");
 		ctx.setAttribute("roomIsFree", false);
@@ -62,27 +66,23 @@ public class MqttClientDaemon implements Runnable, MqttCallback {
 		// Use Madrid's time zone to format the date in
 		df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		df.setTimeZone(TimeZone.getTimeZone("Europe/Madrid"));
-
-		// Initialize mqtt client
-		initMqtt();
-
 	}
 
 	private void initMqtt() throws MqttException {
 
-		// Construct the connection options object that contains connection
-		// parameters
+		// Construct the connection options object that 
+		//contains connection parameters
 		// such as cleanSession and LWT
 		conOpt = new MqttConnectOptions();
-		// conOpt.setUserName(this.userName);
-		// conOpt.setPassword(this.password.toCharArray());
-		conOpt.setCleanSession(true);
-//		conOpt.setAutomaticReconnect(true);	// boh
+		
+		// XXX false forse permette di
+		// non doversi risottoscrivere
+		conOpt.setCleanSession(true); 
+		conOpt.setAutomaticReconnect(true);
 
 		// Construct an MQTT blocking mode client
 		MemoryPersistence persistence = new MemoryPersistence();
-		// MqttClient client = new MqttClient(mqttServerURL, clientID,
-		// persistence);
+
 		mqttclient = new MqttClient(mqttServerURL, clientID, persistence);
 
 		// Set this wrapper as the callback handler
@@ -92,29 +92,22 @@ public class MqttClientDaemon implements Runnable, MqttCallback {
 		mqttclient.connect(conOpt);
 
 		// Subscribe to topic
-		mqttclient.subscribe(topic);
+	//	mqttclient.subscribe(topic);
 
 	}
 
 	public void run() {
 
 		System.out.println("[MqttClientDaemon] Run in execution (thread: " + Thread.currentThread().getName() + ")...");
+		// Initialize mqtt client
+		initialize();
+
 		try {
-			initialize();
+			// Initialize mqtt client
+			initMqtt();
+
 		} catch (MqttException e) {
 
-			// [err] Connection lost (32109) - java.io.EOFException
-			// [err] at
-			// org.eclipse.paho.client.mqttv3.internal.CommsReceiver.run(CommsReceiver.java:146)
-			// [err] at java.lang.Thread.run(Thread.java:745)
-			// [err] Caused by: java.io.EOFException
-			// [err] at
-			// java.io.DataInputStream.readByte(DataInputStream.java:267)
-			// [err] at
-			// org.eclipse.paho.client.mqttv3.internal.wire.MqttInputStream.readMqttWireMessage(MqttInputStream.java:65)
-			// [err] at
-			// org.eclipse.paho.client.mqttv3.internal.CommsReceiver.run(CommsReceiver.java:107)
-			// [err] ... 1 more
 			e.printStackTrace();
 		}
 
@@ -128,102 +121,14 @@ public class MqttClientDaemon implements Runnable, MqttCallback {
 
 	@Override
 	public void connectionLost(Throwable cause) {
-		// Called when the connection to the server has been lost.
-		// An application may choose to implement reconnection
-		// logic at this point. This sample simply exits.
-		// log("Connection to " + brokerUrl + " lost!" + cause);
 
-		// logger.warn("Connection to mqtt broker lost");
-
-		// TODO
-		// vedere
-		// http://stackoverflow.com/questions/33735090/java-eclipse-paho-implementation-auto-reconnect
-		System.out.println("[MqttClientDaemon] Connection to mqtt broker lost.");
-
-		System.out.println(
-					"[MqttClientDaemon] Trying to reconnect in 20 seconds...");
-
-			try {
-				Thread.sleep(20000);
-			} catch (InterruptedException e) {
-
-				e.printStackTrace();
-			}
-
-			try {
-				initMqtt();
-			} catch (MqttException e1) {
-				System.out.println("[MqttClientDaemon] Failed to reconnect to mqtt broker!");
-				System.out.println(e1.getMessage());
-//				e1.printStackTrace();
-			}
-
-		System.out.println("[MqttClientDaemon] Succesfully reconnected to mqtt broker!");
+		System.out.println("[MqttClientDaemon] Connection to mqtt broker lost!");
 
 	}
-	
-	/*
-	@Override
-	public void connectionLost(Throwable cause) {
-		// Called when the connection to the server has been lost.
-		// An application may choose to implement reconnection
-		// logic at this point. This sample simply exits.
-		// log("Connection to " + brokerUrl + " lost!" + cause);
-
-		// logger.warn("Connection to mqtt broker lost");
-
-		// TODO
-		// vedere
-		// http://stackoverflow.com/questions/33735090/java-eclipse-paho-implementation-auto-reconnect
-		System.out.println("[MqttClientDaemon] Connection to mqtt broker lost:");
-		System.out.println("[MqttClientDaemon] " + cause.get);
-
-		while (!mqttclient.isConnected()) {
-
-			System.out.println(
-					"[MqttClientDaemon] Trying to reconnect in 20 seconds...");
-
-			try {
-				Thread.sleep(20000);
-			} catch (InterruptedException e) {
-
-				e.printStackTrace();
-			}
-
-			try {
-				initMqtt();
-			} catch (MqttException e1) {
-				System.out.println("[MqttClientDaemon] Failed to reconnect to mqtt broker!");
-				System.out.println(e1);
-//				e1.printStackTrace();
-			}
-
-		}
-
-		System.out.println("[MqttClientDaemon] Succesfully reconnected to mqtt broker!");
-
-	}
-*/
 
 	@Override
 	public void deliveryComplete(IMqttDeliveryToken arg0) {
-		// Called when a message has been delivered to the
-		// server. The token passed in here is the same one
-		// that was passed to or returned from the original call to publish.
-		// This allows applications to perform asynchronous
-		// delivery without blocking until delivery completes.
-		//
-		// This sample demonstrates asynchronous deliver and
-		// uses the token.waitForCompletion() call in the main thread which
-		// blocks until the delivery has completed.
-		// Additionally the deliveryComplete method will be called if
-		// the callback is set on the client
-		//
-		// If the connection to the server breaks before delivery has completed
-		// delivery of a message will complete after the client has
-		// re-connected.
-		// The getPendingTokens method will provide tokens for any messages
-		// that are still to be delivered.
+
 	}
 
 	@Override
@@ -282,6 +187,26 @@ public class MqttClientDaemon implements Runnable, MqttCallback {
 
 		}
 
+	}
+
+	@Override
+	public void connectComplete(boolean reconnect, String uri) {
+
+		if (reconnect) {
+			System.out.println("[MqttClientDaemon] Automatically Reconnected to "+uri+"!");
+
+		} else
+			System.out.println("[MqttClientDaemon] Connected To "+uri+" for the first time!");
+
+		// Subscribe to topic
+		try {
+			System.out.println("[MqttClientDaemon] Subscribing to " + topic);
+
+			this.mqttclient.subscribe(topic);
+		} catch (MqttException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
