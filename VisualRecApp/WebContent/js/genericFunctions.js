8073270d73e1e7ef3a6a44ef3e63186d743846ca
Luca Crippa <luca_crippa@it.ibm.com>
2017-03-23 15:56:45 +0100

    Modifiche pesanti a tutti i css Modifiche grafiche pesanti a simulate.css

--- VisualRecApp/WebContent/js/genericFunctions.js
+++ VisualRecApp/WebContent/js/genericFunctions.js
@@ -11,15 +11,15 @@
  * @returns Restituisce l'head delle pagine: inclusione di utilities, pagine css e titolo tab browser
  */
 function createHead(){
-	var utilities = ["<meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width, initial-scale=1.0'><meta name='ROBOTS' content='INDEX,FOLLOW'><meta name='GOOGLEBOT' content='ARCHIVE'><link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'>"];
-	var cssfonts = ["<link rel='stylesheet' type='text/css' href='css/fonts.css'/>"]; //css fonts
-	var css0 = ["<link rel='stylesheet' type='text/css' href='css/Reset.css'/>"]; //css reset
-	var css1 = ["<link rel='stylesheet' type='text/css' href='css/default.css'/>"]; //css per le pagine
-	var css2 = ["<link rel='stylesheet' type='text/css' href='css/navbar.css'/>"]; //css per il menu
-	var css3 = ["<link rel='stylesheet' type='text/css' href='css/utilities.css'/>"]; //css per parti comuni della pagina
+	var start = ["<meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width, initial-scale=1.0'><meta name='ROBOTS' content='INDEX,FOLLOW'><meta name='GOOGLEBOT' content='ARCHIVE'><link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'>"];
+	var fon = ["<link rel='stylesheet' type='text/css' href='css/fonts.css'/>"]; //css fonts
+	var def = ["<link rel='stylesheet' type='text/css' href='css/default.css'/>"]; //css per le pagine
+	var str = ["<link rel='stylesheet' type='text/css' href='css/structure.css'/>"]; //css per le pagine
+	var nav = ["<link rel='stylesheet' type='text/css' href='css/navbar.css'/>"]; //css per il menu
+	var uti = ["<link rel='stylesheet' type='text/css' href='css/utilities.css'/>"]; //css per parti comuni della pagina
 	var title = ["<title>Watson VR Test</title>"];
 
-	var headfull = utilities.concat(cssfonts, css0, css1, css2, css3, title);
+	var headfull = start.concat(uti, fon, def, str, nav, uti, title);
 	$('head').append(headfull);
 }
 
